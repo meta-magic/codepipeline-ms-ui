408f991d3e77bdf0c1d0c93db74626881b238e7c
datsgawas <dattaram.gawas@metamagic.in>
2018-09-20 16:50:55 +0530

    dist folder removed

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export * from './app/codepipeline/taskui/taskui.component';
-export * from './app/codepipeline/instance-manager/instance.manager.component';
-export * from './app/codepipeline/sourceCodeConfigration/sourceCode.component';
-export * from './app/codepipeline/notification.component';
-export declare class CodePipeLineMsUiModule {
-    static forRoot(): ModuleWithProviders;
-}
